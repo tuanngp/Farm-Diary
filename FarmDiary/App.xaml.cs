aefe3ca1c10ecac18387f5eb00bd3be8a8367514
Nguyễn Gia Phương Tuấn <nguyengiaphuongtuan@gmail.com>
2024-08-22 21:27:22 +0700

    add icon for navigation menu

--- FarmDiary/App.xaml.cs
+++ FarmDiary/App.xaml.cs
@@ -8,5 +8,19 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.MaximumWidth = 1920;
+            window.MaximumHeight = 1080;
+            window.X = 0;
+            window.Y = 0;
+            window.Width = 1920;
+            window.Height = 1080;
+
+            return window;
+        }
+
     }
 }
